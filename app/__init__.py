d1c82dcc6c654e37d498606f0410c67a18ace81a
huyhoang.ph <phungh67@gmail.com>
2025-12-08 22:48:37 +0100

    feat: fix bugs in scheduler function

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 def create_app():
     app = Flask(__name__)
@@ -8,5 +9,15 @@ def create_app():
     # Register Blueprints
     from app.main.routes import main
     app.register_blueprint(main)
+
+    global scheduler
+    
+    # We only start the scheduler if we are NOT in the reloader's main process
+    # or if we are not in debug mode. This prevents it from running twice.
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true" or not app.debug:
+        from app.services.scheduler import NoteScheduler
+        print("--- APP STARTUP: Initializing Scheduler ---")
+        scheduler = NoteScheduler()
+        scheduler.start()
 
     return app
